d4a1d179f107971891c7b159efe4dda736bd676d
paiementstephanie <stephanie.paiement@rogers.com>
2016-04-12 17:36:20 -0400

    changed transition time

--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
-
+var $body = $('body');
 
 $body.on('hover', '.right-paw', function () {
   $('.right-paw').toggleClass('slide');
+});
+
+$body.on('hover', '.left-paw', function () {
+  $('.left-paw').toggleClass('slide');
 });
