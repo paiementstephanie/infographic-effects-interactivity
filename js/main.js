58221dd645b6f4540ffc82122e66f8e8f9922a65
paiementstephanie <stephanie.paiement@rogers.com>
2016-04-19 17:02:16 -0400

    added jquery waypoints

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 var $body = $('body');
+var $dog = $('.dog');
+var $cat = $('.cat');
 
 $body.on('mouseover', '.right-paw', function () {
   $('.right-paw').addClass('slide');
@@ -16,3 +18,20 @@ $body.on('mouseout', '.right-paw', function () {
   $('.left-paw').removeClass('slide');
 });
 
+$dog.waypoint(function (direction) {
+  if (direction == 'down') {
+    $dog.addClass('js-svg-animate');
+  } else {
+    $dog.removeClass('js-svg-animate');
+  }
+
+}, {offset: '50%' });
+
+$cat.waypoint(function (direction) {
+  if (direction == 'down') {
+    $cat.addClass('js-svg-animate');
+  } else {
+    $cat.removeClass('js-svg-animate');
+  }
+
+}, {offset: '50%' });
